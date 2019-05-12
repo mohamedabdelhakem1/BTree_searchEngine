1c9d66171e6fa6d5aa9d2678cacc71adda14d7d1
mohamedabdelhakem1 <mohamedabdelhakem99@yahoo.com>
2019-05-12 18:31:48 +0400

    delete

--- src/eg/edu/alexu/csd/filestructure/btree/BTree.java
+++ src/eg/edu/alexu/csd/filestructure/btree/BTree.java
@@ -1,11 +1,15 @@
 package eg.edu.alexu.csd.filestructure.btree;
 
+import java.util.List;
+
+import javax.management.RuntimeErrorException;
+
 public class BTree<K extends Comparable<K>, V> implements IBTree<K ,V>  {
 	private IBTreeNode<K, V> root ;
 	private int minDegree ;
 	public BTree(int minDegree) {
 		this.minDegree = minDegree;
-		root = new BTreeNode<>();
+		root = null ;
 	}
 	@Override
 	public int getMinimumDegree() {
@@ -20,20 +24,39 @@ public class BTree<K extends Comparable<K>, V> implements IBTree<K ,V>  {
 
 	@Override
 	public void insert(K key, V value) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public V search(K key) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public boolean delete(K key) {
-		// TODO Auto-generated method stub
+		if(key == null )throw new RuntimeErrorException(new Error());
+		if(root == null) return false;
+		deleteRecursively(root, key);
 		return false;
 	}
-
+	public void deleteRecursively(IBTreeNode<K, V> node , K key) {
+		List<K> keys = node.getKeys();
+		int i = 0;
+		while(i < keys.size() && key.compareTo(keys.get(i)) > 0) {
+			i++;
+		}
+		if (key == keys.get(i)  ){
+			if(node.isLeaf() &&  keys.size() >= minDegree) {
+				keys.remove(i);
+				node.getValues().remove(i);
+				node.setNumOfKeys(keys.size());
+			}else if(node.isLeaf() &&  keys.size() == minDegree-1) {
+				
+			}
+		}else {
+			List<IBTreeNode<K, V>> children = node.getChildren();
+			deleteRecursively(children.get(i), key);
+		}
+	}
+	
 }
