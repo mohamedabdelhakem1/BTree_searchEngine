d20a065ec395382438ef09d548c9f17a365c9d34
Muhammad Salah <msalah.29.10@gmail.com>
2019-05-12 16:26:46 +0200

    nos el insertion

--- src/eg/edu/alexu/csd/filestructure/btree/BTree.java
+++ src/eg/edu/alexu/csd/filestructure/btree/BTree.java
@@ -1,11 +1,18 @@
 package eg.edu.alexu.csd.filestructure.btree;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.management.RuntimeErrorException;
+
 public class BTree<K extends Comparable<K>, V> implements IBTree<K ,V>  {
 	private IBTreeNode<K, V> root ;
-	private int minDegree ;
+	private int minDegree;
+	private int maxDegree;
 	public BTree(int minDegree) {
 		this.minDegree = minDegree;
-		root = new BTreeNode<>();
+		maxDegree = 2*minDegree - 1;
+		root = null;
 	}
 	@Override
 	public int getMinimumDegree() {
@@ -20,7 +27,40 @@ public class BTree<K extends Comparable<K>, V> implements IBTree<K ,V>  {
 
 	@Override
 	public void insert(K key, V value) {
-		// TODO Auto-generated method stub
+		if(key == null) throw new RuntimeErrorException(new Error());
+		if(root == null) {
+			root = new BTreeNode<K, V>();
+			List<K> keys = new ArrayList<K>();
+			keys.add(key);
+			List<V> values = new ArrayList<V>();
+			values.add(value);
+			root.setKeys(keys);
+			root.setValues(values);
+			return;
+		} else {
+			BTreeNode<K, V> z = (BTreeNode<K, V>) root;
+			while(!z.isLeaf()) {
+				List<K> keys = z.getKeys();
+				int i = 0;
+				K k = keys.get(i);
+				while(k != null && k.compareTo(key) < 0) {
+					i++;
+					k = keys.get(i);
+				}
+				z = (BTreeNode<K, V>) z.getChildren().get(i);
+			}
+			if(z.getKeys().size() == maxDegree) {
+				
+			} else if(z.getNumOfKeys() < maxDegree) {
+				List<K> keys = z.getKeys();
+				int pos = 0;
+				while(pos < z.getNumOfKeys() && key.compareTo(keys.get(pos)) > 0) {
+					pos++;
+				}
+				keys.add(pos, key);
+				z.getValues().add(pos, value);
+			}
+		}
 		
 	}
 
